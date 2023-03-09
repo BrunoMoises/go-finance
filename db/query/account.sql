027a7730a49454a19c9e3ad455f73d5873177d71
Bruno <bruno_moises_stz@hotmail.com>
2023-03-09 09:26:25 -0300

    Generate CRUD of accounts using sqlc

---
+++ db/query/account.sql
@@ -0,0 +1,46 @@
+-- name: CreateAccount :one
+insert into accounts (
+    user_id, 
+    category_id, 
+    title,
+    type,
+    description,
+    value,
+    date
+) values (
+    $1, $2, $3, $4, $5, $6, $7
+) returning *;
+
+-- name: GetAccount :one
+select * from accounts 
+where id = $1 limit 1;
+
+-- name: GetAccounts :many
+select 
+    a.user_id, 
+    a.category_id, 
+    a.title,
+    a.type,
+    a.description,
+    a.value,
+    a.date,
+    a.created_at,
+    c.title as category_title
+from accounts a
+left join categories c on c.id = a.category_id 
+where a.user_id = $1 and a.category_id = $2 and a.type = $3 
+and a.title like $4 and a.description like $5 and a.date = $6;
+
+-- name: GetAccountReports :one
+select sum(value) as sum_value from accounts 
+where user_id = $1 and type = $2;
+
+-- name: GetAccountGraph :one
+select count(*) from accounts 
+where user_id = $1 and type = $2;
+
+-- name: UpdateAccount :one
+update accounts set title = $2, description = $3, value = $4 where id = $1 returning *;
+
+-- name: DeleteAccount :exec
+delete from accounts where id = $1;
