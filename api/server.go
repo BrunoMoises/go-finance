e7ff9952c85fdcebfe0f8ce9d0d3548f40180065
Bruno <bruno_moises_stz@hotmail.com>
2023-03-09 20:05:59 -0300

    Create routes of category and account

--- api/server.go
+++ api/server.go
@@ -14,9 +14,26 @@ func NewServer(store *db.SQLStore) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	//User
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
 	router.GET("/user/id/:id", server.getUserById)
+
+	//Category
+	router.POST("/category", server.createCategory)
+	router.GET("/category/id/:id", server.getCategory)
+	router.GET("/category", server.getCategories)
+	router.DELETE("/category/:id", server.deleteCategory)
+	router.PUT("/category/:id", server.updateCategory)
+
+	//Account
+	router.POST("/account", server.createAccount)
+	router.GET("/account/id/:id", server.getAccount)
+	router.GET("/account", server.getAccounts)
+	router.GET("/account/graph/:user_id/:type", server.getAccountGraph)
+	router.GET("/account/reports/:user_id/:type", server.getAccountReports)
+	router.DELETE("/account/:id", server.deleteAccount)
+	router.PUT("/account/:id", server.updateAccount)
 
 	server.router = router
 	return server
