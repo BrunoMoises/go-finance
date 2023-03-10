eaa30758d9fdaaccf5c9ea373368b34d506d25a6
Bruno <bruno_moises_stz@hotmail.com>
2023-03-10 11:50:45 -0300

    Add new filters of search categories api

--- api/category.go
+++ api/category.go
@@ -119,21 +119,91 @@ func (server *Server) getCategories(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 	}
 
-	arg := db.GetCategoriesParams{
-		UserID:      req.UserID,
-		Title:       req.Title,
-		Type:        req.Type,
-		Description: req.Description,
-	}
+	var categories []db.Category
+	filterUserIdAndType := req.UserID > 0 && len(req.Type) > 0
 
-	categories, err := server.store.GetCategories(ctx, arg)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
+	filterAsByUserIdAndType := len(req.Description) == 0 && len(req.Title) == 0 && filterUserIdAndType
+
+	if filterAsByUserIdAndType {
+		arg := db.GetCategoriesByUserIdAndTypeParams{
+			UserID: req.UserID,
+			Type:   req.Type,
+		}
+
+		categoriesByUserIdAndType, err := server.store.GetCategoriesByUserIdAndType(ctx, arg)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.JSON(http.StatusNotFound, errorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
+
+		categories = categoriesByUserIdAndType
+	}
+
+	filterAsByUserIdAndTypeAndDescription := len(req.Description) != 0 && len(req.Title) == 0 && filterUserIdAndType
+	if filterAsByUserIdAndTypeAndDescription {
+		arg := db.GetCategoriesByUserIdAndTypeAndDescriptionParams{
+			UserID:      req.UserID,
+			Type:        req.Type,
+			Description: req.Description,
+		}
+
+		categoriesByUserIdAndTypeAndDescription, err := server.store.GetCategoriesByUserIdAndTypeAndDescription(ctx, arg)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.JSON(http.StatusNotFound, errorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+		}
+
+		categories = categoriesByUserIdAndTypeAndDescription
+	}
+
+	filterAsByUserIdAndTypeAndTitle := len(req.Description) == 0 && len(req.Title) != 0 && filterUserIdAndType
+	if filterAsByUserIdAndTypeAndTitle {
+		arg := db.GetCategoriesByUserIdAndTypeAndTitleParams{
+			UserID: req.UserID,
+			Type:   req.Type,
+			Title:  req.Title,
+		}
+
+		categoriesByUserIdAndTypeAndTitle, err := server.store.GetCategoriesByUserIdAndTypeAndTitle(ctx, arg)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.JSON(http.StatusNotFound, errorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+		}
+
+		categories = categoriesByUserIdAndTypeAndTitle
+	}
+
+	filterAllParam := len(req.Description) != 0 && len(req.Title) != 0 && filterUserIdAndType
+	if filterAllParam {
+		arg := db.GetCategoriesParams{
+			UserID: req.UserID,
+			Type:   req.Type,
+			Title:  req.Title,
+		}
+
+		categoriesByAllFilters, err := server.store.GetCategories(ctx, arg)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.JSON(http.StatusNotFound, errorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+		}
+
+		categories = categoriesByAllFilters
 	}
 
 	ctx.JSON(http.StatusOK, categories)
