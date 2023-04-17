5009bcc6237eadb6174e3d032e131c2482cc6be7
Bruno <bruno_moises_stz@hotmail.com>
2023-04-17 20:49:00 -0300

    fix: configure CORS to FrontEnd request

--- api/server.go
+++ api/server.go
@@ -3,7 +3,6 @@ package api
 import (
 	db "github.com/BrunoMoises/go-finance-api/db/sqlc"
 	"github.com/gin-gonic/gin"
-	cors "github.com/rs/cors/wrapper/gin"
 )
 
 type Server struct {
@@ -11,10 +10,26 @@ type Server struct {
 	router *gin.Engine
 }
 
+func CORSConfig() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(204)
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
 func NewServer(store *db.SQLStore) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
-	router.Use(cors.Default())
+	router.Use(CORSConfig())
 
 	//User
 	router.POST("/user", server.createUser)
